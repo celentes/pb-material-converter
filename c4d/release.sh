8eed2e754f69606e8f4cc2083f6af9762faaf1f7
Tim Talasok <t.talashok@gmail.com>
2020-07-10 19:04:19 +0200

    Slight rework of c4d release

--- c4d/release.sh
+++ c4d/release.sh
@@ -3,7 +3,6 @@ rm -rf c4d_pbmc
 mkdir c4d_pbmc
 cp photobash_material_converter.pyp c4d_pbmc
 mkdir c4d_pbmc/res
-cp ../common/texture_mapping.py .
-python2 -m compileall arnold.py octane.py vray.py UI.py texture_mapping.py -b
-rm texture_mapping.py
+python2 -m compileall arnold.py octane.py vray.py UI.py ../common/texture_mapping.py -b
+mv ../common/texture_mapping.pyc .
 mv *.pyc c4d_pbmc/res
