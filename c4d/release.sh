419451780f2b1afce17af65a74ea641c96851345
Tim Talasok <t.talashok@gmail.com>
2020-07-19 21:33:57 +0200

    Put changelog into release.sh

--- c4d/release.sh
+++ c4d/release.sh
@@ -3,6 +3,7 @@ rm -rf c4d_pbmc
 mkdir c4d_pbmc
 cp photobash_material_converter.pyp c4d_pbmc
 cp icon_pbmc.png c4d_pbmc
+cp CHANGELOG.txt c4d_pbmc
 mkdir c4d_pbmc/res
 python2 -m compileall physical.py arnold.py octane.py vray.py UI.py ../common/texture_mapping.py -b
 mv ../common/texture_mapping.pyc .
